814647b18df5e1de4815306eab495f40e4b4ac5b
Josh Shavers <shaversj@gmail.com>
2023-09-14 07:46:08 -0500

    Fix a11y issues

--- src/Submit.jsx
+++ src/Submit.jsx
@@ -2,8 +2,8 @@
 const Submit = () => {
     return (
         <button type={"submit"} className={" bg-primary-green w-full p-4 rounded-lg  "}>
-            <p className={"uppercase text-[16px] text-white tracking-wider"}>Claim Your Free
-                Trial</p>
+            <span className={"uppercase text-[16px] text-white tracking-wider"}>Claim Your Free
+                Trial</span>
         </button>
     );
 };
