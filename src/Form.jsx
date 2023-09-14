5dd45000011a05d272d8c2e25ba21731cf764701
Josh Shavers <shaversj@gmail.com>
2023-09-14 07:09:35 -0500

    Add email validation

--- src/Form.jsx
+++ src/Form.jsx
@@ -16,13 +16,14 @@ const Form = () => {
                 <div
                     className={"bg-white px-5 lg:min-w-[540px] rounded-lg first:pt-6 last:pb-6 drop-shadow-2xl shadow-[0px_8px_0px_0px_#00000024] lg:first:p-10  "}>
 
-                    <Input placeHolder={"First Name"} inputFieldName={"firstName"}
+                    <Input placeHolder={"First Name"} isRequired={true} inputFieldName={"firstName"}
                            errorMessage={"First Name cannot be empty"}/>
-                    <Input placeHolder={"Last Name"} inputFieldName={"lastName"}
+                    <Input placeHolder={"Last Name"} isRequired={true} inputFieldName={"lastName"}
                            errorMessage={"Last Name cannot be empty"}/>
-                    <Input placeHolder={"Email Address"} inputFieldName={"emailAddress"}
-                           errorMessage={"Looks like this is not an email"}/>
-                    <Input placeHolder={"Password"} inputFieldName={"password"}
+                    <Input placeHolder={"Email Address"} isRequired={true} inputFieldName={"emailAddress"}
+                           errorMessage={"Looks like this is not an email"}
+                           inputPattern={/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i}/>
+                    <Input placeHolder={"Password"} isRequired={true} inputFieldName={"password"}
                            errorMessage={"Password cannot be empty"}/>
 
                     <Submit/>
--- src/Input.jsx
+++ src/Input.jsx
@@ -1,7 +1,7 @@
 import iconError from "./assets/images/icon-error.svg";
 import {useFormContext} from "react-hook-form";
 
-const Input = ({placeHolder, inputFieldName, errorMessage}) => {
+const Input = ({placeHolder, isRequired, inputFieldName, errorMessage, inputPattern}) => {
 
     const {register, formState} = useFormContext()
 
@@ -9,7 +9,7 @@ const Input = ({placeHolder, inputFieldName, errorMessage}) => {
             <input
                 className={"focus:outline-none focus:border-accent-blue border border-outline-gray w-full placeholder-dark-blue placeholder-opacity-80 p-[.93rem] lg:p-[1.03rem] lg:pl-8 rounded-lg placeholder:font-medium placeholder:font-Poppins font-Poppins font-medium text-[14.5px]  placeholder:text-[14.5px]"}
                 placeholder={placeHolder} id={"firstName"} type={"text"} name={"first name"}
-                {...register(inputFieldName, {required: true})}
+                {...register(inputFieldName, {required: isRequired,...inputPattern && {pattern: inputPattern}})}
             />
             {formState.errors?.[inputFieldName] && (<>
                     <div className={"text-right text-xs text-primary-red mt-2"} role="alert">{errorMessage}</div>
